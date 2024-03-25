73a1f389d440bf7ef3cb908d9dea86ef388ea0dd
Reetam Majumder <50930364+reetamm@users.noreply.github.com>
2024-03-25 16:06:42 -0400

    example

--- example.R
+++ example.R
@@ -33,3 +33,31 @@ u1 <- runif(n)
 y1 <- q1(u1,y0)
 u2 <- runif(n)
 y2 <- q2(u2,y0,y1)
+X <- cbind(u1,y0)
+library(SPQR)
+control <- list(iter = 300, warmup = 200, thin = 1)
+fit <- SPQR(X = y0, Y = y1, method = "MLE", control = control, normalize = TRUE, verbose = T)
+head(X)
+
+qout <- u1[y0==0]
+head(x_test)
+x_test[,2] = 1
+
+qf.mle  <- predict(fit, X = 1, type = "QF",tau=qout)
+qf.mle  <- predict(fit, X = 1, type = "QF",tau=runif(1000))
+# hist(qf.mle)
+# hist(y1[y0==1])
+
+summary(qf.mle)
+summary(y1[y0==1])
+summary(y1[y0==0])
+
+d0 <-density(y1[y0==0]) 
+d1 <-density(y1[y0==1]) 
+d2 <- density(qf.mle)
+plot(d0,col=1,ylim=range(c(d0$y,d1$y)),ylab="Y1",main="Y1")
+lines(d1,col=2)
+lines(d2,col=3)
+legend("topright",c("Model","Observations",'Bias-corrected'),col=1:3,lwd=2,bty="n")
+
+
