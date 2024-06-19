94d6e741517292a4243ac52a27b8ae5d210e2ded
Reetam Majumder <rmajumd3@ncsu.edu>
2024-06-18 22:27:44 -0400

    plots

--- summary_spacetime.R
+++ summary_spacetime.R
@@ -3,7 +3,11 @@ library(ggplot2)
 library(scales)
 library(lubridate)
 library(GpGp)
-state = 'TN'
+library(usmap)
+library(transport)
+library(gridExtra)
+state = 'SE'
+
 gcm.long = read.csv(paste0('data/',state,'_gcm_data.csv'))
 obs.long = read.csv(paste0('data/',state,'_obs_data.csv'))
 
@@ -14,17 +18,26 @@ str(grid.no)
 coords = aggregate(gcm.long[,2:3],by = list(grid.no), FUN = mean)
 coords = coords[,-1]
 head(coords)
+coords$lon = coords$lon - 360
+GeoLocations <- usmap_transform(coords)
+
+# p1 = plot_usmap(regions ="state", include = c('UT','CO','AZ','NM')) + geom_sf(data = GeoLocations)
+# p2 = plot_usmap(regions ="state", include = 
+#                     c('Indiana','Ohio','West Virginia','Kentucky',
+#                       'Virginia','Tennessee','North Carolina', 'South Carolina',
+#                       'Mississippi','Alabama','Georgia')) + geom_sf(data = GeoLocations)
+
 table(grid.no)
 set.seed(303)
 vecchia.order = order_maxmin(coords,lonlat = T)
-y1.cors.0 = NA
-y2.cors.0 = NA
-y1y2.cors.0 = NA
-y1.cors.1 = NA
-y2.cors.1 = NA
-y1y2.cors.1 = NA
-daysinmonth = c(31,28,31,30,31,30,31,31,30,31,30,31)
-
+# y1.cors.0 = NA
+# y2.cors.0 = NA
+# y1y2.cors.0 = NA
+# y1.cors.1 = NA
+# y2.cors.1 = NA
+# y1y2.cors.1 = NA
+# daysinmonth = c(31,28,31,30,31,30,31,31,30,31,30,31)
+# 
 # cal.data = vector('list',12)
 # for(mnth in 1:12){
 #     cal.array = array(dim = c(daysinmonth[mnth]*64,6,25))
@@ -36,15 +49,15 @@ daysinmonth = c(31,28,31,30,31,30,31,31,30,31,30,31)
 #         n0 = length(gcm.long$pr[vecchia.order==loc & gcm.months==mnth]); n1 = length(obs.long$pr[vecchia.order==loc & gcm.months==mnth])
 #         n = n0 + n1
 #         y0 <- rep(1:0,each=n0)
-#         
+# 
 #         y11 = y1[y0==1]
 #         x11 = y11[c(n1,1:(n1-1))]
 #         y21 = y2[y0==1]
 #         x21 = y21[c(n1,1:(n1-1))]
-#         
-#         envname = paste0('fits/fits_space/fits_temp_m',mnth,'_l',loc,'.RDS')
+# 
+#         envname = paste0('fits/',state,'/spacetime/fits_temp_m',mnth,'_l',loc,'.RDS')
 #         qf.y1.mle.ts <- readRDS(envname)
-#         envname = paste0('fits/fits_space/fits_prcp_m',mnth,'_l',loc,'.RDS')
+#         envname = paste0('fits/',state,'/spacetime/fits_prcp_m',mnth,'_l',loc,'.RDS')
 #         qf.y2.mle.ts <- readRDS(envname)
 #         y1.cors.0 = c(y1.cors.0,cor(qf.y1.mle.ts[y0==0][-1],qf.y1.mle.ts[y0==0][-n0]))
 #         y1.cors.1 = c(y1.cors.1,cor(cbind(y11,x11))[1,2])
@@ -64,14 +77,38 @@ daysinmonth = c(31,28,31,30,31,30,31,31,30,31,30,31)
 # }
 # 
 # save(y1.cors.0,y1.cors.1,y2.cors.0,y2.cors.1,y1y2.cors.0,y1y2.cors.1,cal.data,
-#            file = 'summary_space.RData')
-load('summary_time.RData')
+#            file = paste0('summary_spacetime_',state,'.RData'))
+load(paste0('summary_spacetime_',state,'.RData'))
 eachmonth = rep(NA,12)
 # for(i in 1:12){
 #     eachmonth[i] = dim(cal.data[[i]])[1]
 #     cal.data[[i]][,4:6,] = exp(cal.data[[i]][,4:6,])-1
 # }
-cal.array = cal.data[[6]]
+wasdist = array(dim = c(25,2))
+# for(mnth in 1:12){
+#     cal.array = cal.data[[mnth]]
+#     for(loc in 1:25){
+#         wasdist[loc,1,mnth] <- wasserstein1d(cal.array[,2,loc],cal.array[,3,loc])
+#         wasdist[loc,2,mnth] <- wasserstein1d(cal.array[,5,loc],cal.array[,6,loc])
+#     }
+#     
+# }
+
+cal.array = do.call(abind::abind,c(cal.data,along=1))
+for(loc in 1:25){
+    wasdist[loc,1] <- wasserstein1d(cal.array[,2,loc],cal.array[,3,loc])
+    wasdist[loc,2] <- wasserstein1d(cal.array[,5,loc],cal.array[,6,loc])
+}
+summary(wasdist)
+apply(wasdist,2,sd)
+metrics = data.frame(coords,wasdist,vecchia.order)
+ggplot(metrics,aes(x=lon,y=lat,fill=X1)) + geom_raster() + coord_equal() +
+    geom_text(aes(label=round(X1,2)),col='white') + ggtitle(paste(state,'temp')) +
+    theme(legend.position = 'none')
+ggplot(metrics,aes(x=lon,y=lat,fill=X2)) + geom_raster() + coord_equal() +
+    geom_text(aes(label=round(X2,2)),col='white') + ggtitle(paste(state,'prcp')) +
+    theme(legend.position = 'none')
+
 mnth=12
 loc=1
 maxy = 1.01*max(cal.array[1:31,4:6,loc])
@@ -80,16 +117,24 @@ plot(1:31,cal.array[1:31,4,loc],type = 'b',col=1,pch=20,ylim = c(miny,maxy))
 lines(1:31,cal.array[1:31,5,loc],type = 'b',col=2,pch=20)
 lines(1:31,cal.array[1:31,6,loc],type = 'b',col=3,pch=20)
 
-png('autocorr_space.png',width = 1200,height = 400)
-par(mfrow=c(1,3))
-plot(y1.cors.0,y1.cors.1,pch=20,xlab = 'calibrated',ylab = 'observed',main = 'temp autocorrelations')
+season = rep(1:4,each=75)
+png(paste0('autocorr_spacetime_',state,'.png'),width = 800,height = 400)
+par(mfrow=c(1,2))
+plot(y1.cors.0[-1],y1.cors.1[-1],pch=20,xlab = 'calibrated',ylab = 'observed',
+     col=season,main = 'temp autocorrelations')
+legend('topleft',c('JFM','AMJ','JAS','OND'),col=1:4,pch=20,pt.cex = 2)
 abline(0,1)
-plot(y2.cors.0,y2.cors.1,pch=20,xlab = 'calibrated',ylab = 'observed',main = 'prcp autocorrelations')
+plot(y2.cors.0[-1],y2.cors.1[-1],pch=20,xlab = 'calibrated',ylab = 'observed',
+     col = season, main = 'prcp autocorrelations')
+legend('topleft',c('JFM','AMJ','JAS','OND'),col=1:4,pch=20,pt.cex = 2)
 abline(0,1)
-plot(y1y2.cors.0,y1y2.cors.1,pch=20,xlab = 'calibrated',ylab = 'observed',main = 'cross correlations')
-abline(0,1)
+# plot(y1y2.cors.0,y1y2.cors.1,pch=20,xlab = 'calibrated',ylab = 'observed',main = 'cross correlations')
+# abline(0,1)
 par(mfrow=c(1,1))
 dev.off()
+
+### rmse of cross correlations
+sqrt(mean((y1y2.cors.0[-1] - y1y2.cors.1[-1])**2))
 
 q1 = 0.9
 q2 = 0.9
@@ -110,121 +155,87 @@ for(mnth in 1:12)
         
         
     }
-png('tailprob_space.png',width = 1200,height = 400)
-par(mfrow=c(1,3))
-tmp = apply(prcp.tail,3,c)
-dim(tmp)
-xmax = max(tmp)
-xmin = min(tmp)
-plot(density(tmp[,2]),xlab = 'probability',main = 'precip',xlim = c(xmin,xmax))
-lines(density(tmp[,1]),col=2)
-abline(v=1-q2)
-legend('topright',c('Calibrated','GCM'),col=1:2,lwd=2,bty="n")
-
-
+# png('tailprob_space.png',width = 1200,height = 400)
+# par(mfrow=c(1,3))
+# tmp = apply(prcp.tail,3,c)
+# dim(tmp)
+# xmax = max(tmp)
+# xmin = min(tmp)
+# plot(density(tmp[,2]),xlab = 'probability',main = 'precip',xlim = c(xmin,xmax))
+# lines(density(tmp[,1]),col=2)
+# abline(v=1-q2)
+# legend('topright',c('Calibrated','GCM'),col=1:2,lwd=2,bty="n")
+# 
+# 
+# tmp = apply(temp.tail,3,c)
+# dim(tmp)
+# xmax = max(tmp)
+# xmin = min(tmp)
+# plot(density(tmp[,1]),xlab = 'probability',main = 'temp',xlim = c(xmin,xmax),col=2)
+# lines(density(tmp[,2]))
+# abline(v=1-q1)
+# legend('topright',c('Calibrated','GCM'),col=1:2,lwd=2,bty="n")
+# 
+# tmp = apply(joint.tail,3,c)
+# dim(tmp)
+# xmax = max(tmp)
+# xmin = min(tmp)
+# plot(density(tmp[,2]),xlab = 'probability',main = 'joint',xlim = c(xmin,xmax),col=2)
+# lines(density(tmp[,1]))
+# lines(density(tmp[,3]))
+# abline(v=1-q1)
+# legend('topright',c('Calibrated','GCM'),col=1:2,lwd=2,bty="n")
+# par(mfrow=c(1,1))
+# dev.off()
+png(paste0('tailprob_spacetime_',state,'.png'),width = 800,height = 400)
+par(mfrow=c(1,2))
 tmp = apply(temp.tail,3,c)
-dim(tmp)
-xmax = max(tmp)
-xmin = min(tmp)
-plot(density(tmp[,1]),xlab = 'probability',main = 'temp',xlim = c(xmin,xmax),col=2)
-lines(density(tmp[,2]))
-abline(v=1-q1)
-legend('topright',c('Calibrated','GCM'),col=1:2,lwd=2,bty="n")
+plot(tmp[,1],tmp[,2],xlab = 'uncalibrated',ylab = 'calibrated',pch=20,
+     main = 'Temp exceedance probability above 0.90 quantile',col=season)
+legend('topright',c('JFM','AMJ','JAS','OND'),col=1:4,pch=20,pt.cex = 2)
+abline(h=0.1)
 
-tmp = apply(joint.tail,3,c)
-dim(tmp)
-xmax = max(tmp)
-xmin = min(tmp)
-plot(density(tmp[,2]),xlab = 'probability',main = 'joint',xlim = c(xmin,xmax),col=2)
-lines(density(tmp[,1]))
-lines(density(tmp[,3]))
-abline(v=1-q1)
-legend('topright',c('Calibrated','GCM'),col=1:2,lwd=2,bty="n")
-par(mfrow=c(1,1))
+tmp = apply(prcp.tail,3,c)
+plot(tmp[,1],tmp[,2],xlab = 'uncalibrated',ylab = 'calibrated',pch=20,
+     main = 'Prcp exceedance probability above 0.90 quantile',col=season)
+legend('topleft',c('JFM','AMJ','JAS','OND'),col=1:4,pch=20,pt.cex = 2)
+abline(h=0.1)
 dev.off()
-
-# plot(tmp[,3],tmp[,2])
-# plot(tmp[,1],tmp[,2])
 # abline(0,1)
 # 
 # coords = cbind(coords,vecchia.order)
 # names(coords)
 # ggplot(coords,aes(x=lon,y=lat,col='white'))+geom_tile()+geom_text(aes(label=vecchia.order))
 
-# correls = matrix(NA,300*12,8)
-# count = 0
-# for(mnth in 1:12){
-# cal.array = cal.data[[mnth]] 
-# print(mnth)
-# for(i in 1:24)
-#     for(j in (i+1):25){
-#         count = count+1
-#        loc1 = coords[vecchia.order==i,-3] 
-#        loc2 = coords[vecchia.order==j,-3] 
-#        correls[count,7] = as.numeric(sqrt((loc1[1]-loc2[1])**2 + (loc1[2]-loc2[2])**2))
-#        correls[count,8] = mnth
-#        for(k in 1:6)
-#            correls[count,k] = cor(cal.array[,k,c(1,j)])[1,2]
-# }
-# }
-# par(mfrow=c(2,3))
-# plot(correls[,7],correls[,1],col=correls[,8],xlim = c(0,8),pch=20,
-#      xlab = 'Distance',ylab = 'Correlation',main = 'GCM temp')
-# legend('topright',month.abb,col=1:12,lwd=2,bty="n")
-# 
-# plot(correls[,7],correls[,2],col=correls[,8],xlim = c(0,8),pch=20,
-#      xlab = 'Distance',ylab = 'Correlation',main = 'Calibrated temp')
-# legend('topright',month.abb,col=1:12,lwd=2,bty="n")
-# 
-# plot(correls[,7],correls[,3],col=correls[,8],xlim = c(0,8),pch=20,
-#      xlab = 'Distance',ylab = 'Correlation',main = 'Observed temp')
-# legend('topright',month.abb,col=1:12,lwd=2,bty="n")
-# 
-# 
-# plot(correls[,7],correls[,4],col=correls[,8],xlim = c(0,8),pch=20,
-#      xlab = 'Distance',ylab = 'Correlation',main = 'GCM prcp')
-# legend('topright',month.abb,col=1:12,lwd=2,bty="n")
-# 
-# plot(correls[,7],correls[,5],col=correls[,8],xlim = c(0,8),pch=20,
-#      xlab = 'Distance',ylab = 'Correlation',main = 'Calibrated prcp')
-# legend('topright',month.abb,col=1:12,lwd=2,bty="n")
-# 
-# plot(correls[,7],correls[,6],col=correls[,8],xlim = c(0,8),pch=20,
-#      xlab = 'Distance',ylab = 'Correlation',main = 'Observed prcp')
-# legend('topright',month.abb,col=1:12,lwd=2,bty="n")
-# par(mfrow=c(1,1))
-# 
-# plot(correls[,2],correls[,3],col=correls[,7]+1,pch=20,
-#      xlab = 'Calibrated',ylab = 'Observed',main = 'Temp')
-# abline(0,1)
-# points(correls[,2],correls[,1],col=alpha(1,0.05),pch=20,
-#      xlab = 'Calibrated',ylab = 'Observed',main = 'Temp')
-# legend('topleft','GCM',col=1,lwd=2,bty="n")
-# 
-# plot(correls[,5],correls[,6],col=correls[,7]+1,pch=20,
-#      xlab = 'Calibrated',ylab = 'Observed',main = 'Prcp')
-# abline(0,1)
-# points(correls[,5],correls[,4],col=alpha(1,0.05),pch=20,
-#        xlab = 'Calibrated',ylab = 'Observed',main = 'Prcp')
-# legend('topleft','GCM',col=1,lwd=2,bty="n")
-
-# creating correlation matrix
-png('spatcorr_time.png',width = 800,height = 400)
-corr_mat1 <- corr_mat2 <- corr_mat3 <- corr_mat4 <- NA
-par(mfrow=c(1,2))
-for(i in 1:12){
-    cal.array <- cal.data[[i]]
-    corr_mat1 <- c(corr_mat1, gdata::upperTriangle(cor(cal.array[,2,])))
-    corr_mat2 <- c(corr_mat2, gdata::upperTriangle(cor(cal.array[,3,])))
-    
-    corr_mat3 <- c(corr_mat3, gdata::upperTriangle(cor(cal.array[,5,])))
-    corr_mat4 <- c(corr_mat4, gdata::upperTriangle(cor(cal.array[,6,])))
-    
+correls = matrix(NA,300*12,8)
+count = 0
+for(mnth in 1:12){
+cal.array = cal.data[[mnth]]
+print(mnth)
+for(i in 1:24)
+    for(j in (i+1):25){
+        count = count+1
+       loc1 = coords[vecchia.order==i,-3]
+       loc2 = coords[vecchia.order==j,-3]
+       correls[count,7] = as.numeric(sqrt((loc1[1]-loc2[1])**2 + (loc1[2]-loc2[2])**2))
+       correls[count,8] = mnth
+       for(k in 1:6)
+           correls[count,k] = cor(cal.array[,k,c(i,j)])[1,2]
 }
-plot(corr_mat1,corr_mat2,pch=20,xlab = 'calibrated',ylab='observed',main = 'temp spatial correlations')
-abline(0,1)
-plot(corr_mat3,corr_mat4,pch=20,xlab = 'calibrated',ylab='observed',main = 'prcp spatial correlations')
-abline(0,1)
+}
+season = rep(1:4,each=900)
 
-par(mfrow=c(1,1))
+png(paste0('spatcorr_spacetime_',state,'.png'),width = 800,height = 400)
+par(mfrow=c(1,2))
+plot(correls[,2],correls[,1],col=alpha(1,0.4),pch=3,
+     xlab = 'Calibrated',ylab = 'Observed',main = 'Temp spatial correlations')
+points(correls[,2],correls[,3],col=alpha(season,0.2),pch=20)
+abline(0,1)
+legend('topleft',c('GCM','Obs'),pch = c(3,20))
+
+plot(correls[,5],correls[,4],col=alpha(1,0.4),pch=3,
+     xlab = 'Calibrated',ylab = 'Observed',main = 'Prcp spatial correlations')
+points(correls[,5],correls[,6],col=alpha(season,0.2),pch=20)
+abline(0,1)
+legend('topleft',c('GCM','Obs'),pch = c(3,20))
 dev.off()
